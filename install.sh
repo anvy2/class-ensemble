f449e83b4c222951819b812fd4ca1d516578059e
Anshul Patel <anshul2679@gmail.com>
2020-11-12 14:49:37 +0530

    changed install.sh to install python2 instead of python3

--- install.sh
+++ install.sh
@@ -1,12 +1,21 @@
-echo "Make sure to install python 3.8+ before proceeding with this script"
+echo "Make sure to install python 2.7 before proceeding with this script"
 echo "Enter full path for the python environment: "
 read location
 echo "Entered path is `$location`"
-python -m venv $location
+virtualenv -p python2 $location
 env_name=`basename $location`
 echo "Activating environment"
 source $env_name/bin/activate
-echo "Upgrading Pip"
+python_location=`which python | grep $location/bin/python`
+location=$location/bin/python
+if [[ $python_location == $location ]]
+then
+	echo "Proceeding further. Environment successfully created"
+else
+	echo "Entered wrong path of location. Try again and delete the environment folder if created at wrong location"
+	exit 1
+	fi
+echo "Upgrading PIP"
 pip install --upgrade pip
 echo "Installing modules and its dependencies"
 pip install -r requirements.txt
